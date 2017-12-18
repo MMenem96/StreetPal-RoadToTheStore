cfd03691c3856066c38b52d57589ea3de6db4074
LmisKmal <la1177@fayoum.edu.eg>
2017-12-18 05:44:13 +0200

    Implementing puch notification with FCM

---
+++ app/src/main/java/com/sharekeg/streetpal/FirebaseCloudMessaging/MyFirebaseInstanceIDService.java
@@ -0,0 +1,31 @@
+package com.sharekeg.streetpal.FirebaseCloudMessaging;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Lmis on 12/8/2017.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+
+        //send the Instance ID token to the server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+}
---
+++ app/src/main/java/com/sharekeg/streetpal/FirebaseCloudMessaging/MyJobService.java
@@ -0,0 +1,29 @@
+package com.sharekeg.streetpal.FirebaseCloudMessaging;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+
+/**
+ * Created by Lmis on 12/8/2017.
+ */
+
+public class MyJobService extends JobService {
+
+    private static final String TAG = "MyJobService";
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        Log.d(TAG, "Performing long running task in scheduled job");
+        // TODO(developer): add long running task here.
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return false;
+    }
+
+}
